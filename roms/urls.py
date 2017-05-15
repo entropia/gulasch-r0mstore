e8e17aff16c416bb4f941ad53807e42f75d42c45
LongHairedHacker <sebastian@sebastians-site.de>
2017-05-16 00:14:17 +0200

    Fixed slugs

--- roms/urls.py
+++ roms/urls.py
@@ -4,11 +4,11 @@ from roms.views import RomListJson, RomDetailViewJson, RomList, RomDetailView, R
 
 urlpatterns = [
     url(r'^json/list/$', RomListJson.as_view()),
-    url(r'^json/list/(?P<tag>[a-zA-Z0-9-]+)/$', RomListJson.as_view()),
+    url(r'^json/list/(?P<tag>[-\w]+)/$', RomListJson.as_view()),
     url(r'^json/details/(?P<id>[0-9]+)/$', RomDetailViewJson.as_view()),
 
     url(r'^list/$', RomList.as_view(), name = 'romlist'),
-    url(r'^list/(?P<tag>[a-zA-Z0-9-]+)/$', RomList.as_view(), name = 'romlist'),
+    url(r'^list/(?P<tag>[-\w]+)/$', RomList.as_view(), name = 'romlist'),
     url(r'^details/(?P<id>[0-9]+)/$', RomDetailView.as_view(), name = 'romdetails'),
 
     url(r'^new/$', RomCreateView.as_view(), name = 'romcreate'),
