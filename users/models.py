1f89ddb66bf3b5ee815b057e21c9834706ee0970
LongHairedHacker <sebastian@sebastians-site.de>
2017-05-24 21:39:54 +0200

    Added missing validators for twitter/github usernames Fixed urls

--- users/models.py
+++ users/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.core.validators import RegexValidator
+
+twitter_validator = RegexValidator(regex=r'^[a-zA-Z0-9_]+$', message='Bitte nur den Benutzername, ohne führendes @.')
+github_validator = RegexValidator(regex=r'^[a-z\d](?:[a-z\d]|-(?=[a-z\d]))*$', message='Bitte nur den Benutzername, nicht die Profil-Url.')
 
 class User(AbstractUser):
-    github = models.CharField('github', max_length=128, blank=True)
-    twitter = models.CharField('twitter', max_length=128, blank=True)
+    github = models.CharField('Github Benutzername', max_length=38, blank=True, validators=[github_validator])
+    twitter = models.CharField('Twitter Benutzername', max_length=15, blank=True, validators=[twitter_validator])
